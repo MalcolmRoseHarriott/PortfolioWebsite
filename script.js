43f4028a5d286ef916687f146fdcc20cc5711689
MalcolmRH <malcolm.roseharriott@uoit.net>
2021-06-27 21:07:57 -0400

    Improved mobile website responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos <= currentScrollPos && $(window).width() < 680) {
+    document.getElementById("navbar").style.top = "-180px";
+  } else {
+    document.getElementById("navbar").style.top = "0";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 $(document).ready(function () 
 {
     $('.submit').click(function (event) 
